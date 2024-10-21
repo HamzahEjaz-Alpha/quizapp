dafe4262ff3c9e98ebee74bcf760fc7ec78c8f7c
ahmedalphasquad123 <ahmedali.alphasquad@gmail.com>
2024-10-21 15:49:13 +0500

    feat: error handling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [gridSize, setGridSize] = useState(0);
   const [grid, setGrid] = useState([]);
   const [isSolved, setIsSolved] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const generateShuffledGrid = useCallback((size) => {
     const numbers = Array.from({ length: size * size }, (_, i) => i + 1);
@@ -16,7 +17,11 @@ function App() {
   }, []);
 
   const handleGenerateGrid = () => {
-    if (gridSize > 0) {
+    if (gridSize < 1) {
+      setErrorMessage("Number can't be smaller than 1");
+      return;
+    } else if (gridSize > 0) {
+      setErrorMessage("")
       const shuffledGrid = generateShuffledGrid(gridSize);
       setGrid(shuffledGrid);
       setIsSolved(false);
@@ -36,7 +41,7 @@ function App() {
     const updatedGrid = [...grid];
     [updatedGrid[fromIndex], updatedGrid[toIndex]] = [updatedGrid[toIndex], updatedGrid[fromIndex]];
     setGrid(updatedGrid);
-    
+
     setIsSolved(updatedGrid.every((num, idx) => num === idx + 1));
   };
 
@@ -44,14 +49,23 @@ function App() {
     <div className="App flex flex-col items-center py-8">
       <h1 className="text-4xl font-bold text-gray-800 mb-8">Puzzle App</h1>
 
-      
+
       <div className="mb-8 flex space-x-4">
         <input
           type="number"
           value={gridSize}
-          onChange={(e) => setGridSize(Number(e.target.value))}
+          onChange={(e) => {
+            const value = Number(e.target.value);
+            setGridSize(value);
+            if (value < 1) {
+              setErrorMessage("Number can't be smaller than 1");
+            } else {
+              setErrorMessage('');
+            }
+          }}
           className="border-2 border-gray-300 rounded p-2 w-32 text-center"
           placeholder="Grid size"
+          min={1}
         />
         <button
           onClick={handleGenerateGrid}
@@ -60,8 +74,10 @@ function App() {
           Generate Grid
         </button>
       </div>
-
-      {grid?.length>0 && <div
+      {errorMessage && (
+        <span className="text-red-500">{errorMessage}</span>
+      )}
+      {grid?.length > 0 && <div
         className="grid gap-4 mb-8 border-2 border-double rounded-md border-gray-300 p-2"
         style={{ gridTemplateColumns: `repeat(${gridSize}, 100px)` }}
       >
@@ -78,8 +94,8 @@ function App() {
           </div>
         ))}
       </div>
-}
-     
+      }
+
       {isSolved && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
           <div className="bg-white p-6 rounded shadow-lg text-center">
